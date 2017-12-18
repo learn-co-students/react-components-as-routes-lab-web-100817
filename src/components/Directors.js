e3d54b2be253c9fc85aecc70f45808d057848158
MatteBru <mbruni616@gmail.com>
2017-12-18 15:30:58 -0500

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,9 +2,18 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+  let dcts = directors.map(d =>
+    <div key={d.name}>
+      <h3>{d.name}</h3>
+      <ul>
+        {d.movies.map(m => <li>{m}</li>)}
+      </ul>
+    </div>
+  )
   return (
     <div>
-      {/*{code here}*/}
+      <h1>"Directors Page"</h1>
+      {dcts}
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,8 +3,14 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <div>
-      {/*{code here}*/}
+    <div className="navbar">
+      <NavLink to='/' exact>Home</NavLink>
+      <br></br>
+      <NavLink to='/movies' exact>Movies</NavLink>
+      <br></br>
+      <NavLink to='/actors' exact>Actors</NavLink>
+      <br></br>
+      <NavLink to='/directors' exact>Directors</NavLink>
     </div>
   );
 };
