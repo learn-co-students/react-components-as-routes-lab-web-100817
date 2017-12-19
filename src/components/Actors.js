000cfa251e9a7532af39a10adf57bd5ed33c10ba
Liam Killion <liam.killion@gmail.com>
2017-12-19 14:36:37 -0500

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import { actors } from '../data';
+import React from "react";
+import { actors } from "../data";
+
+const actorList = actors.map(actor => {
+  return (
+    <div>
+      <p>Name: {actor.name}</p>
+      <ul>
+        Movies:
+        {actor.movies.map(movie => {
+          return <li>{movie}</li>;
+        })}
+      </ul>
+    </div>
+  );
+});
 
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actorList}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import { movies } from '../data';
+import React from "react";
+import { movies } from "../data";
+
+let movieList = movies.map(movie => {
+  return (
+    <div>
+      <p>Title: {movie.title}</p>
+      <p>Run Time: {movie.time}</p>
+      <ul>
+        Genres:
+        {movie.genres.map(genre => {
+          return <li>{genre}</li>;
+        })}
+      </ul>
+    </div>
+  );
+});
 
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movieList}
     </div>
   );
 };
