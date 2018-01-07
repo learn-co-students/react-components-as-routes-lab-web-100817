a216306c6ac48bbd7dae25dfcb487380670749d5
Abe Rudy <aberudy@gmail.com>
2018-01-07 16:58:21 -0500

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,19 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map(actor => {
+        return (
+          <div>
+            <p>{actor.name}</p>
+            <ul>
+              {actor.movies.map(movie => {
+                return <li>{movie}</li>;
+              })}
+            </ul>
+          </div>
+        );
+      })}
     </div>
   );
 };
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,9 +4,21 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map(director => {
+        return (
+          <div>
+            <p>{director.name}</p>
+            <ul>
+              {director.movies.map(movie => {
+                return <li>{movie}</li>;
+              })}
+            </ul>
+          </div>
+        );
+      })}
     </div>
   );
-}
+};
 
-export default Directors
+export default Directors;
